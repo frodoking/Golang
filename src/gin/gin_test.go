4520842fa106b4b463315715aa46010b8208bbe2
frodo <awangyun8@gmail.com>
2015-06-25 18:22:39 +0800

    add gin test

---
+++ src/gin/gin_test.go
@@ -0,0 +1,62 @@
+package gin
+
+import (
+	"reflect"
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	SetMode(TestMode)
+}
+
+func TestCreateEngine(t *testing.T) {
+	router := New()
+	assert.Equal(t, "/", router.BasePath)
+	assert.Equal(t, router.engine, router)
+	assert.Empty(t, router.Handlers)
+
+	assert.Panics(t, func() { router.addRoute("", "/", HandlersChain{func(_ *Context) {}}) })
+	assert.Panics(t, func() { router.addRoute("GET", "a", HandlersChain{func(_ *Context) {}}) })
+	assert.Panics(t, func() { router.addRoute("GET", "/", HandlersChain{}) })
+}
+
+func TestCreateDefaultRouter(t *testing.T) {
+	router := Default()
+	assert.Len(t, router.Handlers, 2)
+}
+
+func TestNoRouteWithoutGlobalHandlers(t *testing.T) {
+	var middleware0 HandlerFunc = func(c *Context) {}
+	var middleware1 HandlerFunc = func(c *Context) {}
+
+	router := New()
+
+	router.NoRoute(middleware0)
+	assert.Nil(t, router.Handlers)
+	assert.Len(t, router.noRoute, 1)
+	assert.Len(t, router.allNoRoute, 1)
+
+	compareFunc(t, router.noRoute[0], middleware0)
+	compareFunc(t, router.allNoRoute[0], middleware0)
+
+	router.NoRoute(middleware1, middleware0)
+	assert.Len(t, router.noRoute, 2)
+	assert.Len(t, router.allNoRoute, 2)
+	compareFunc(t, router.noRoute[0], middleware1)
+	compareFunc(t, router.allNoRoute[0], middleware1)
+	compareFunc(t, router.noRoute[1], middleware0)
+	compareFunc(t, router.allNoRoute[1], middleware0)
+}
+
+func compareFunc(t *testing.T, a, b interface{}) {
+	sf1 := reflect.ValueOf(a)
+	sf2 := reflect.ValueOf(b)
+
+	if sf1.Pointer() != sf2.Pointer() {
+		t.Error("different functions")
+	}
+}
